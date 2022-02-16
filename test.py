d8e7af1ee6be974c97fc109d5adf3afa5bf207d6
Fayaz Ahmed <fayaz.hopper@gmail.com>
2022-02-16 13:50:26 -0800

    now working with reddit API

---
+++ test.py
@@ -0,0 +1,16 @@
+import requests
+
+redditurl = "https://www.reddit.com/"
+redditData = {'grant_type': 'password', 'username': 'NearquadFarquad', 'password': 'Inheritance1'}
+auth = requests.auth.HTTPBasicAuth('i9gheT9P680rW97oiuhXWA', 'dJ1iwMFSJEzuCGGyVR_KG0zF6Hp8TQ')
+redditReq = requests.post(redditurl + 'api/v1/access_token', data = redditData, headers={'user-agent': 'script by Fayaz Ahmed'}, auth = auth)
+redditD = redditReq.json()
+token = redditD['access_token']
+
+redditToken = 'bearer ' + token
+apiurl = "https://oauth.reddit.com/r/all/search"
+headers = {'Authorization': redditToken, 'User-Agent': 'Script by Fayaz Ahmed'}
+payload = {'q': 'crossword', 'limit': 1, 'sort': 'relevance'}
+response = requests.get(apiurl, headers = headers, params = payload)
+
+print(response.json()['data']['children'][0]['data']['permalink'])
--- wiki.py
+++ wiki.py
@@ -1,6 +1,9 @@
 from webbrowser import get
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
+from searchtweets import ResultStream, gen_rule_payload, load_credentials, collect_results
+
 import requests
 
 app = FastAPI()
@@ -9,11 +12,56 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
 async def root():
+    source = input("Reddit, Wikipedia or Twitter(NO TWITTER)?: ")
     search = input("Search Query: ") #This will be an input string
     results = []
-    response = requests.get("https://en.wikipedia.org/w/api.php?action=opensearch&search=" + search + "&limit=10&namespace=0&format=json")
-    for i in range(10):
-        results.append([response.json()[1][i], response.json()[3][i]])
+    limit = 1
+
+    if source.lower() == 'wikipedia':
+        wikiurl = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + search + "&limit=10&namespace=0&format=json"
+        response = requests.get(wikiurl)
+
+        for i in range(limit):
+            results.append([response.json()[1][i], response.json()[3][i]])
+
+
+    elif source.lower() == 'reddit':
+        redditurl = "https://www.reddit.com/"
+        redditData = {'grant_type': 'password', 'username': 'FayazTestsAPI', 'password': 'APITestingWithFayaz'}
+        auth = requests.auth.HTTPBasicAuth('qTgintKTLlcLhma-KR9ZAA', 'SpMdvn3rH8b8zbqZgQPxjL-s-IQLeA')
+        redditReq = requests.post(redditurl + 'api/v1/access_token',
+                                    data = redditData, headers={'user-agent': 'script by Fayaz Ahmed'},
+                                    auth = auth)
+        redditD = redditReq.json()
+        redditToken = 'bearer' + redditD['access_token']
+        redditurl = 'https://oauth.reddit.com/r/all/search'
+        redHeaders = {'Authorization': redditToken, 'User-Agent': 'Script by Fayaz Ahmed'}
+        redParams = {'q': search, 'limit': limit, 'sort': 'top'}
+        response = requests.get(redditurl, headers = redHeaders, params = redParams)
+
+        for i in range(limit):
+            full = response.json()['data']['children'][i]['data']
+            results.append([full['title'], 'https://reddit.com' + full['permalink'], full['thumbnail']])
+
+
+    '''
+    elif source.lower() == 'twitter':
+        twiturl = "https://api.twitter.com/1.1/search/tweets.json?q=" + search + "&count=" + str(limit) + "&result_type=popular"
+        twitAPI = "UUJH8fe6OkF7RaglDurbjssq4"
+        twitSecret = "YXXhwpNEpe8gBb57Ugmj4D6BjEB1pf5ch9PPbH05oB7XGJasTL"
+        twitBearer = "AAAAAAAAAAAAAAAAAAAAACcbZQEAAAAAk4wW2OMMtPjKa0Gob3ruYsdKlAI%3DKHJQBUX6HinobsueJynN5bQGF0sLuYG0WbCKeZfQQpgaJe9nMa"
+        twitReq = requests.get(twiturl, headers = headers)
+
+        enterpriseSearchArgs = load_credentials(STUFF)
+        rule = gen_rule_payload(search, results_per_call=limit)
+        tweets = collect_results(rule, max_results=limit, result_stream_args=enterpriseSearchArgs)
+
+
+
+        results.append['','']
+        #print(twitReq.json())
+    '''
+
 
     for res in results:
         print(res[0] + ": " + res[1])
